a5175516358ffa444e38a98028c96888bf161679
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-05-29 14:16:53 +0100

    Update app

--- app.ts
+++ app.ts
@@ -31,7 +31,7 @@ app.listen(PORT, async () => {
 });
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.log(`${'\x1b[31m'}${err.message}${'/x1b[0m]'}`);
+  console.log(`${'\x1b[31m'}${err.message}${'\x1b[0m]'}`);
   return res
     .status(500)
     .send({ success: false, statusCode: 500, message: err.message });
